cfda6ea80feb4fe902b1b29b620073dffe034adc
Diego <diegocosta.contato2021@gmail.com>
2024-06-03 11:55:19 -0300

    Iniciando projeto. Setando formatações base: tamanho texto, font, cores,line height e imagens(logos)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,38 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors:
+      {
+        "primary":"#00868F",
+        "secondary":"#00B4D8",
+        "third":"#03045E",
+        "white":"#EEEEEE",
+        "dark":"#262626",
+      },
+      borderColor: {
+        "primary":"#00868F",
+        "secondary":"#00B4D8",
+        "third":"#03045E",
+        "white":"#EEEEEE",
+        "black":"#262626",
+      },
+      fontSize: {
+        sm: '0.8rem',
+        base: '1.13rem',
+        h1: '2.25rem',
+        h2: '2rem',
+        h3:'1.75rem',
+        xl: '1.50rem',
+      },
+      padding:{
+        'button':'0.50rem 2rem'
+      },
+      lineHeight: {
+          '3': '3rem',
+      },
     },
   },
   plugins: [],
 };
+
 export default config;
